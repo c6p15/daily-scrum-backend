902a84397a5d2997b7816da01a918c1c205f3ade
c6p15 <doubleak077@gmail.com>
2025-07-03 12:45:57 +0700

    Initial commit

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/users.controller.js");
-const auth = require("../middleware/auth.js"); // your JWT middleware
+const auth = require("../middleware/auth.js"); 
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
